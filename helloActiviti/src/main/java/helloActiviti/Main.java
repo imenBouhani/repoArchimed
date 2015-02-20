ede92de4d60ef32035d180ade3cdee5aa5cd9feb
imen <imen@Imen>
2015-02-20 12:31:31 +0100

    Signed-off-by: imen <imen@Imen>

--- helloActiviti/src/main/java/helloActiviti/Main.java
+++ helloActiviti/src/main/java/helloActiviti/Main.java
@@ -3,7 +3,7 @@ package helloActiviti;
 public class Main {
 
 	public static void main(String[] args) {
-	System.out.println("hello word");
+	System.out.println("hello word modifier en local");
 	System.out.println("hello word2");
 	}
 
